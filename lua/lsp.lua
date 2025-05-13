143d3c16a23d07c54be51f4c1b85385d5b235d9e
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-05-13 08:45:37 +0300

    Added ensure_installed LSPs

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,12 +1,19 @@
 -- somewhere after you require mason-lspconfig
 local keymaps = require("keybinds")
 
-require("mason-lspconfig").setup_handlers({
-  function(server)
-    require("lspconfig")[server].setup({
-      on_attach = function(_, bufnr)
-        keymaps.lsp(bufnr) 
-      end,
-    })
-  end,
+require("mason-lspconfig").setup({
+  -- automatically install these servers on first run
+  ensure_installed = { "lua_ls", "rust_analyzer" },
+
+  -- handler for each installed server: apply our on_attach callback
+  handlers = {
+    function(server)
+      require("lspconfig")[server].setup({
+        on_attach = function(_, bufnr)
+          print("✔️ LSP attached to buffer", bufnr)
+          keymaps.lsp(bufnr)
+        end,
+      })
+    end,
+  },
 })
