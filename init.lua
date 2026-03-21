97b26ce186d2d72b605d413ab990bb92f3ddeb81
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-03-21 11:28:53 +0200

    Added highlight on hover

--- init.lua
+++ init.lua
@@ -1,19 +1,53 @@
 require("config.lazy")
 
-vim.opt.number = true        
-vim.opt.relativenumber = true 
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.updatetime = 250
 vim.cmd.colorscheme("habamax")
 
 local themes = { "shine", "habamax" }
 local current = vim.fn.index(themes, vim.g.colors_name) + 1
 if current == 0 then
-  current = 1  
+  current = 1
 end
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
-    vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if not client then
+      return
+    end
+
+    if client:supports_method("textDocument/inlayHint") then
+      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+
+    if client:supports_method("textDocument/documentHighlight") then
+      local group = vim.api.nvim_create_augroup("lsp-document-highlight-" .. args.buf, { clear = true })
+
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = args.buf,
+        group = group,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufLeave" }, {
+        buffer = args.buf,
+        group = group,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd("LspDetach", {
+        buffer = args.buf,
+        group = group,
+        callback = function()
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds({ group = group, buffer = args.buf })
+        end,
+      })
+    end
   end,
 })
 
@@ -23,6 +57,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ async = false })
   end,
 })
+
+require('lualine').setup()
 
 local function toggle_theme()
   current = current % #themes + 1
