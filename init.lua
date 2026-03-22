511d46d360b77de2e4cae850251b3a1b6312bb38
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-03-22 17:06:24 +0200

    Autocomplete

--- init.lua
+++ init.lua
@@ -13,8 +13,21 @@ vim.lsp.enable("buf_ls")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menuone", "noinsert", "popup" }
 vim.opt.updatetime = 250
 vim.cmd.colorscheme("habamax")
+
+vim.keymap.set("i", "<CR>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-y>"
+  end
+
+  return "<CR>"
+end, {
+  expr = true,
+  silent = true,
+  desc = "Confirm completion or insert newline",
+})
 
 local themes = { "shine", "habamax" }
 local current = vim.fn.index(themes, vim.g.colors_name) + 1
@@ -60,6 +73,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     if client:supports_method("textDocument/typeDefinition") then
       map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
+    end
+
+    if client:supports_method("textDocument/completion") then
+      if client.name == "rust-analyzer" then
+        local chars = {}
+        for i = 32, 126 do
+          table.insert(chars, string.char(i))
+        end
+
+        client.server_capabilities.completionProvider =
+          client.server_capabilities.completionProvider or {}
+        client.server_capabilities.completionProvider.triggerCharacters = chars
+      end
+
+      vim.lsp.completion.enable(true, client.id, args.buf, {
+        autotrigger = true,
+      })
+
+      map("i", "<C-Space>", function()
+        vim.lsp.completion.get()
+      end, "Trigger completion")
     end
 
     if client:supports_method("textDocument/codeAction") then
