b957763c94e793754cd14d187f157a8affdcde20
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-03-30 11:13:46 +0300

    Added custom rust-analyzer setup for custom target

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,8 +1,42 @@
+local function rust_analyzer_cmd()
+  local default_toolchain = vim.system({ 'rustup', 'default' }, { text = true }):wait()
+  local toolchain = (default_toolchain.stdout or ''):match('^(%S+)')
+
+  if toolchain then
+    local rust_analyzer = vim.system(
+      { 'rustup', 'which', 'rust-analyzer', '--toolchain', toolchain },
+      { text = true }
+    ):wait()
+    local path = vim.trim(rust_analyzer.stdout or '')
+
+    if rust_analyzer.code == 0 and path ~= '' then
+      return { path }
+    end
+  end
+
+  return { 'rust-analyzer' }
+end
+
 return {
-    { 'rust-lang/rust.vim' },
-    {
-	  'mrcjkb/rustaceanvim',
-	  version = '^8', -- Recommended
-	  lazy = false, -- This plugin is already lazy
-	}
+  { 'rust-lang/rust.vim' },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^8', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    init = function()
+      ---@type rustaceanvim.Opts
+      vim.g.rustaceanvim = {
+        server = {
+          cmd = rust_analyzer_cmd,
+          default_settings = {
+            ['rust-analyzer'] = {
+              cargo = {
+                features = 'all',
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
