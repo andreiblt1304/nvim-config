c792683734e1f3e876627bf269c616ee9c366cfb
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-03-30 11:14:00 +0300

    Added black background for astrodark

--- lua/plugins/astrovim.lua
+++ lua/plugins/astrovim.lua
@@ -8,6 +8,18 @@ return {
         light = "astrojupiter",
         dark = "astrodark",
       },
+      palettes = {
+        astrodark = {
+          ui = {
+            base = "#000000",
+            inactive_base = "#000000",
+            float = "#000000",
+          },
+          term = {
+            background = "#000000",
+          },
+        },
+      },
     },
     config = function(_, opts)
       require("astrotheme").setup(opts)
