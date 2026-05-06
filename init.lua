83db83e0082174f82aa41285ce385712d31e48c6
Andrei Baltariu <andreibaltariu1304@gmail.com>
2026-05-06 12:36:53 +0300

    Modified printed current file

--- init.lua
+++ init.lua
@@ -181,5 +181,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ async = false })
   end,
 })
-
-require('lualine').setup()
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,43 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local startup_cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":p")
+
+    local function startup_relative_filename()
+      local name = vim.api.nvim_buf_get_name(0)
+
+      if name == "" then
+        return "[No Name]"
+      end
+
+      if name:match("^%w+://") then
+        return name
+      end
+
+      local absolute = vim.fn.fnamemodify(name, ":p")
+
+      if vim.fs and vim.fs.relpath then
+        local ok, relative = pcall(vim.fs.relpath, startup_cwd, absolute)
+
+        if ok and relative then
+          return relative
+        end
+      end
+
+      if absolute:sub(1, #startup_cwd) == startup_cwd then
+        return absolute:sub(#startup_cwd + 1)
+      end
+
+      return vim.fn.fnamemodify(absolute, ":~")
+    end
+
+    require("lualine").setup({
+      sections = {
+        lualine_c = {
+          startup_relative_filename,
+        },
+      },
+    })
+  end,
 }
